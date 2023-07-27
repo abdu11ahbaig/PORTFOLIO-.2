79ea3653d5139255e34b56cf2f4aec6633f75f24
abdu11ahbaig <baigabdullahtariq111y@gmail.com>
2023-07-27 17:56:30 +0500

    Initial commit

---
+++ src/components/Who.jsx
@@ -0,0 +1,113 @@
+import React, { Suspense } from "react";
+import styled from "styled-components";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Cube from "./Cube";
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 60px;
+  }
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+
+  @media only screen and (max-width: 768px) {
+    align-items: center;
+    text-align: center;
+  }
+`;
+
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h2`
+  color: #da4ea2;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+`;
+
+const ButtonLink = styled.a`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 120px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+`;
+
+const Who = () => {
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <Canvas camera={{ position: [5, 5, 5], fov: 25 }}>
+            <Suspense fallback={null}>
+              <ambientLight intensity={0.5} />
+              <directionalLight position={[3, 2, 1]} />
+              <Cube />
+              <OrbitControls enableZoom={false} autoRotate />
+            </Suspense>
+          </Canvas>
+        </Left>
+        <Right>
+          <Title>Think out of the box</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>Who am I</Subtitle>
+          </WhatWeDo>
+          <Desc>a creative developer with a passion for the art.</Desc>
+          <ButtonLink href="https://github.com/abdu11ahbaig">
+            See our works
+          </ButtonLink>
+        </Right>
+      </Container>
+    </Section>
+  );
+};
+
+export default Who;
